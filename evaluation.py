29e5f50c55f5b8096183972650caa7b64d86cf97
Carine Dengler <git@carine-dengler.de>
2024-06-14 13:51:01 +0200

    feat: use max_length for perplexity to avoid overflow

--- evaluation.py
+++ evaluation.py
@@ -289,7 +289,7 @@ def _eval_perplexity(model, tokenizer, input_, log_perplexity=False):
     :returns: perplexities
     :rtype: dict
     """
-    encodings = tokenizer(input_, return_tensors="pt")
+    encodings = tokenizer(input_, return_tensors="pt", max_length=1024)
     # https://huggingface.co/docs/transformers/perplexity#example-calculating-perplexity-with-gpt-2-in--transformers
     max_length = 1024
     stride = 512
